89fa976778045836e3a5be4ebbf564a3369c9772
t4sk <tsk.nakamura@gmail.com>
2023-08-06 20:34:23 +0900

    variables example

---
+++ src/pages/variables/index.html.ts
@@ -0,0 +1,40 @@
+// metadata
+export const version = "0.8.13"
+export const title = "Variables"
+export const description = "Variables in Sway"
+export const codes = [
+    {
+        fileName: "variables",
+        code: "Y29udHJhY3Q7CgphYmkgTXlDb250cmFjdCB7CiAgICBmbiB0ZXN0X2Z1bmMoKSAtPiB1NjQ7Cn0KCmltcGwgTXlDb250cmFjdCBmb3IgQ29udHJhY3QgewogICAgZm4gdGVzdF9mdW5jKCkgLT4gdTY0IHsKICAgICAgICAvLyBJbW11dGFibGUKICAgICAgICAvLyAwIDw9IHU2NCA8PSAyKio2NCAtIDEKICAgICAgICBsZXQgeCA9IDU7CiAgICAgICAgLy8gQ2Fubm90IHJlLWFzc2lnbiB4IHRvIGFub3RoZXIgdmFsdWUKICAgICAgICAvLyB4ID0gNjsKCiAgICAgICAgLy8gTXV0YWJsZQogICAgICAgIGxldCBtdXQgeSA9IDU7CiAgICAgICAgeSA9IDY7CgogICAgICAgIC8vIFR5cGUgYW5ub3RhdGlvbnMKICAgICAgICBsZXQgaTogdTMyID0gMTIzOwoKICAgICAgICB5CiAgICB9Cn0K",
+    },
+]
+
+const html = `<p>Examples of variables in Sway</p>
+<pre><code class="language-rust">contract;
+
+abi MyContract {
+    <span class="hljs-keyword">fn</span> <span class="hljs-title function_">test_func</span>() <span class="hljs-punctuation">-&gt;</span> <span class="hljs-type">u64</span>;
+}
+
+<span class="hljs-keyword">impl</span> <span class="hljs-title class_">MyContract</span> <span class="hljs-keyword">for</span> <span class="hljs-title class_">Contract</span> {
+    <span class="hljs-keyword">fn</span> <span class="hljs-title function_">test_func</span>() <span class="hljs-punctuation">-&gt;</span> <span class="hljs-type">u64</span> {
+        <span class="hljs-comment">// Immutable</span>
+        <span class="hljs-comment">// 0 &lt;= u64 &lt;= 2**64 - 1</span>
+        <span class="hljs-keyword">let</span> <span class="hljs-variable">x</span> = <span class="hljs-number">5</span>;
+        <span class="hljs-comment">// Cannot re-assign x to another value</span>
+        <span class="hljs-comment">// x = 6;</span>
+
+        <span class="hljs-comment">// Mutable</span>
+        <span class="hljs-keyword">let</span> <span class="hljs-keyword">mut </span><span class="hljs-variable">y</span> = <span class="hljs-number">5</span>;
+        y = <span class="hljs-number">6</span>;
+
+        <span class="hljs-comment">// Type annotations</span>
+        <span class="hljs-keyword">let</span> <span class="hljs-variable">i</span>: <span class="hljs-type">u32</span> = <span class="hljs-number">123</span>;
+
+        y
+    }
+}
+</code></pre>
+`
+
+export default html
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,18 +1,19 @@
 import component_abi_decode from "./pages/abi-decode"
 import component_abi_encode from "./pages/abi-encode"
-import component_cheatsheet from "./pages/cheatsheet"
-import component_error_handling from "./pages/error-handling"
 import component_account_types from "./pages/account-types"
-import component_imports from "./pages/imports"
-import component_hello_sway from "./pages/hello-sway"
-import component_ownership from "./pages/ownership"
-import component_events from "./pages/events"
-import component_tokens from "./pages/tokens"
+import component_cheatsheet from "./pages/cheatsheet"
 import component_contract_calls from "./pages/contract-calls"
-import component_interface from "./pages/interface"
-import component_initialization from "./pages/initialization"
-import component_defi_staking_contract from "./pages/defi/staking-contract"
 import component_defi_flashloans from "./pages/defi/flashloans"
+import component_defi_staking_contract from "./pages/defi/staking-contract"
+import component_error_handling from "./pages/error-handling"
+import component_events from "./pages/events"
+import component_hello_sway from "./pages/hello-sway"
+import component_imports from "./pages/imports"
+import component_initialization from "./pages/initialization"
+import component_interface from "./pages/interface"
+import component_ownership from "./pages/ownership"
+import component_tokens from "./pages/tokens"
+import component_variables from "./pages/variables"
 import component_ from "./pages"
 
 interface Path {
@@ -32,70 +33,74 @@ interface Route {
 }
 
 const routes: Route[] = [
-  {
-    path: "/abi-decode",
-    component: component_abi_decode,
-  },
-  {
-    path: "/abi-encode",
-    component: component_abi_encode,
-  },
-  {
-    path: "/cheatsheet",
-    component: component_cheatsheet,
-  },
-  {
-    path: "/error-handling",
-    component: component_error_handling,
-  },
-  {
-    path: "/account-types",
-    component: component_account_types,
-  },
-  {
-    path: "/imports",
-    component: component_imports,
-  },
-  {
-    path: "/hello-sway",
-    component: component_hello_sway,
-  },
-  {
-    path: "/initialization",
-    component: component_initialization,
-  },
-  {
-    path: "/ownership",
-    component: component_ownership,
-  },
-  {
-    path: "/events",
-    component: component_events,
-  },
-  {
-    path: "/tokens",
-    component: component_tokens,
-  },
-  {
-    path: "/contract-calls",
-    component: component_contract_calls,
-  },
-  {
-    path: "/interface",
-    component: component_interface,
-  },
-  {
-    path: "/defi/flashloans",
-    component: component_defi_flashloans,
-  },
-  {
-    path: "/defi/staking-contract",
-    component: component_defi_staking_contract,
-  },
-  {
-    path: "",
-    component: component_,
-  },
+    {
+        path: "/abi-decode",
+        component: component_abi_decode
+    },
+    {
+        path: "/abi-encode",
+        component: component_abi_encode
+    },
+    {
+        path: "/account-types",
+        component: component_account_types
+    },
+    {
+        path: "/cheatsheet",
+        component: component_cheatsheet
+    },
+    {
+        path: "/contract-calls",
+        component: component_contract_calls
+    },
+    {
+        path: "/defi/flashloans",
+        component: component_defi_flashloans
+    },
+    {
+        path: "/defi/staking-contract",
+        component: component_defi_staking_contract
+    },
+    {
+        path: "/error-handling",
+        component: component_error_handling
+    },
+    {
+        path: "/events",
+        component: component_events
+    },
+    {
+        path: "/hello-sway",
+        component: component_hello_sway
+    },
+    {
+        path: "/imports",
+        component: component_imports
+    },
+    {
+        path: "/initialization",
+        component: component_initialization
+    },
+    {
+        path: "/interface",
+        component: component_interface
+    },
+    {
+        path: "/ownership",
+        component: component_ownership
+    },
+    {
+        path: "/tokens",
+        component: component_tokens
+    },
+    {
+        path: "/variables",
+        component: component_variables
+    },
+    {
+        path: "",
+        component: component_
+    },
 ]
 
 export default routes
